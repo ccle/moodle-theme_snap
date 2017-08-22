fa015d0a3cce8124616f17ff2dc73ed23a52f229
Emily Chen <emilyqychen@ucla.edu>
2017-08-22 10:21:20 -0700

    CCLE-6839 - Removed junk in footer.

--- layout/footer.php
+++ layout/footer.php
@@ -86,8 +86,8 @@ if(!empty($socialmedialinks)) {
     echo '<div id="snap_socialmedia_links">'.$socialmedialinks.'</div>';
 }
 ?>
-
-<div id='mrooms-footer' class="helplink text-right">
+<!--START UCLA MOD: CCLE-6839-remove junk in footer.-->
+<!--<div id='mrooms-footer' class="helplink text-right">
     <small>
     <?php
     if ($OUTPUT->page_doc_link()) {
@@ -97,12 +97,14 @@ if(!empty($socialmedialinks)) {
     ?>
     <br/><?php echo $poweredbyrunby ?>
     </small>
-</div>
+</div>-->
 <!-- close mrooms footer -->
 <div id="page-footer">
-<?php echo $OUTPUT->lang_menu(); ?>
+<!-- <?php echo $OUTPUT->lang_menu(); ?> -->
 <?php echo $OUTPUT->standard_footer_html(); ?>
 </div>
+
+<!--END UCLA MOD: CCLE-6839.-->
 </footer>
 <?php echo $OUTPUT->standard_end_of_body_html() ?>
 <!-- bye! -->
